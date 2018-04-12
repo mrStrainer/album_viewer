cf393fe5b1c033b5214ec952d6a028a237c3a9e7
Attila Szita <atee88@gmail.com>
2018-04-13 00:31:15 +0200

    added reducers

--- reducers/index.js
+++ reducers/index.js
@@ -1,1 +1,74 @@
 import { combineReducers } from 'redux'
+
+import {
+	REQUEST_ALBUM,
+	REQUEST_SEARCH,
+	RECEIVE_ALBUM,
+	RECEIVE_SEARCH,
+	LOGIN,
+	LOGOUT
+} from '../actions';
+
+const currentAlbum = (state = {
+	isFetching: false,
+	album:{}
+}, action) => {
+	switch (action.type) {
+		case REQUEST_ALBUM:
+			return {
+				...state,
+				isFetching:true
+			}
+		case RECEIVE_ALBUM:
+			return {
+				...state,
+				isFetching:false
+				album:action.album
+			}
+		default:
+			return state;
+	}
+}
+
+const currentSearch = (state = {
+	isFetching:false,
+	results:[],
+	total:0
+}, action) => {
+	switch (action.type) {
+		case REQUEST_SEARCH:
+			return {
+				...state,
+				isFetching:true,
+			}
+		case RECEIVE_SEARCH:
+			return {
+				...state,
+				isFetching:false,
+				results:action.results.albums,
+				total:action.results.total
+			}
+		default:
+			return state;
+	}
+}
+
+const login = (state ={
+	isLoggedIn:false
+}, action) => {
+	switch (action.type) {
+		case LOGIN:
+			return {
+				...state,
+				isLoggedIn:true,
+				token:action.token,
+			}
+		case LOGOUT:
+			return {
+				...state,
+				isLoggedIn:false
+			}
+		default:
+			return state;
+	}
+}
